b0d5fd9d6ec9ebb282d15b72de4c63905b33bb26
Yasuhito Takamiya <yasuhito@gmail.com>
2022-07-12 09:05:03 +0900

    tetris

---
+++ constants.js
@@ -0,0 +1,110 @@
+const COLS = 10;
+const ROWS = 20;
+const BLOCK_SIZE = 30;
+const NO_OF_HIGH_SCORES = 10;
+const HIGH_SCORES = 'highScores';
+const COLORS = ['cyan', 'blue', 'orange', 'yellow', 'green', 'purple', 'red'];
+const SHAPES = [
+  [[0, 0, 0, 0], [1, 1, 1, 1], [0, 0, 0, 0], [0, 0, 0, 0]],
+  [[2, 0, 0], [2, 2, 2], [0, 0, 0]],
+  [[0, 0, 3], [3, 3, 3], [0, 0, 0]],
+  [[4, 4], [4, 4]],
+  [[0, 5, 5], [5, 5, 0], [0, 0, 0]],
+  [[0, 6, 0], [6, 6, 6], [0, 0, 0]],
+  [[7, 7, 0], [0, 7, 7], [0, 0, 0]]
+];
+const POINTS = {
+  SINGLE: 100,
+  DOUBLE: 300,
+  TRIPLE: 500,
+  TETRIS: 800,
+  SOFT_DROP: 1,
+  HARD_DROP: 2
+}
+Object.freeze(POINTS);
+
+const LINES_PER_LEVEL = 10;
+const LEVEL = {
+  0: 800,
+  1: 720,
+  2: 630,
+  3: 550,
+  4: 470,
+  5: 380,
+  6: 300,
+  7: 220,
+  8: 130,
+  9: 100,
+  10: 80,
+  11: 80,
+  12: 80,
+  13: 70,
+  14: 70,
+  15: 70,
+  16: 50,
+  17: 50,
+  18: 50,
+  19: 30,
+  20: 30,
+  // 29+ is 20ms
+}
+Object.freeze(LEVEL);
+
+const canvas = document.getElementById('board');
+const ctx = canvas.getContext('2d');
+const canvasNext = document.getElementById('next');
+const ctxNext = canvasNext.getContext('2d');
+
+// Calculate size of canvas from constants.
+ctx.canvas.width = COLS * BLOCK_SIZE;
+ctx.canvas.height = ROWS * BLOCK_SIZE;
+
+// Size canvas for four blocks.
+ctxNext.canvas.width = 4 * BLOCK_SIZE;
+ctxNext.canvas.height = 4 * BLOCK_SIZE;
+
+// Scale blocks
+ctx.scale(BLOCK_SIZE, BLOCK_SIZE);
+ctxNext.scale(BLOCK_SIZE, BLOCK_SIZE);
+
+
+const KEY = {
+  SPACE: 32,
+  LEFT: 37,
+  UP: 38,
+  RIGHT: 39,
+  DOWN: 40
+}
+Object.freeze(KEY);
+
+let accountValues = {
+  score: 0,
+  lines: 0,
+  level: 0
+}
+
+function updateAccount(key, value) {
+  let element = document.getElementById(key);
+  if (element) {
+    element.textContent = value;
+  }
+}
+
+let account = new Proxy(accountValues, {
+  set: (target, key, value) => {
+    target[key] = value;
+    updateAccount(key, value);
+    return true;
+  }
+});
+
+const moves = {
+  [KEY.LEFT]:  (p) => ({ ...p, x: p.x - 1 }),
+  [KEY.RIGHT]: (p) => ({ ...p, x: p.x + 1 }),
+  [KEY.DOWN]:  (p) => ({ ...p, y: p.y + 1 }),
+  [KEY.UP]:    (p) => board.rotate(p),
+  [KEY.SPACE]:  (p) => ({ ...p, y: p.y + 1 }),
+};
+
+let requestId = null;
+let board = null;
---
+++ main.js
@@ -0,0 +1,126 @@
+
+showHighScores();
+
+function handleKeyPress(event) {
+  // Stop the event from bubbling.
+  event.preventDefault();
+
+  if (moves[event.keyCode]) {
+    // Get new state of piece
+    let p = moves[event.keyCode](board.piece);
+
+    if (event.keyCode === KEY.SPACE) {
+      // Hard drop
+      while (board.valid(p)) {
+        board.piece.move(p);
+        account.score += POINTS.HARD_DROP;
+        p = moves[KEY.SPACE](board.piece);
+      }
+    }
+
+    if (board.valid(p)) {
+      board.piece.move(p);
+      if (event.keyCode === KEY.DOWN) {
+        account.score += POINTS.SOFT_DROP;
+      }
+    }
+  }
+}
+
+function addEventListener() {
+  document.removeEventListener('keydown', handleKeyPress);
+  document.addEventListener('keydown', handleKeyPress);
+}
+
+function draw() {
+  const { width, height } = ctx.canvas;
+  ctx.clearRect(0, 0, width, height);
+
+  board.draw();
+  board.piece.draw();
+}
+
+function resetGame() {
+  account.score = 0;
+  account.lines = 0;
+  account.level = 0;
+  board = new Board(ctx, ctxNext);
+  time = { start: performance.now(), elapsed: 0, level: LEVEL[0] };
+}
+
+function play() {
+  resetGame();
+  addEventListener();
+
+  // If we have an old game running then cancel it
+  if (requestId) {
+    cancelAnimationFrame(requestId);
+  }
+  time.start = performance.now();
+  animate();
+}
+
+time = { start: 0, elapsed: 0, level: 1000 };
+
+function animate(now = 0) {
+  // Update elapsed time.
+  time.elapsed = now - time.start;
+
+  // If elapsed time has passed time for current level
+  if (time.elapsed > time.level) {
+    // Restart counting from now
+    time.start = now;
+
+    if (!board.drop()) {
+      gameOver();
+      return;
+    }
+  }
+
+  draw();
+  requestId = requestAnimationFrame(animate);
+}
+
+function gameOver() {
+  cancelAnimationFrame(requestId);
+  ctx.fillStyle = 'black';
+  ctx.fillRect(1, 3, 8, 1.2);
+  ctx.font = '1px Arial';
+  ctx.fillStyle = 'red';
+  ctx.fillText('GAME OVER', 1.8, 4);
+
+  checkHighScore(account.score);
+}
+
+function checkHighScore(score) {
+  const highScores = JSON.parse(localStorage.getItem(HIGH_SCORES)) || [];
+
+  const lowestScore = highScores[NO_OF_HIGH_SCORES - 1]?.score ?? 0;
+
+  if (score > lowestScore) {
+    saveHighScore(score, highScores);
+    showHighScores();
+  }
+}
+
+function saveHighScore(score, highScores) {
+  const name = prompt('You got a highscore! Enter name:');
+
+  const newScore = { score, name };
+
+  highScores.push(newScore);
+  highScores.sort((a, b) => b.score - a.score);
+  highScores.splice(NO_OF_HIGH_SCORES);
+
+  localStorage.setItem(HIGH_SCORES, JSON.stringify(highScores));
+};
+
+function showHighScores() {
+  const highScores = JSON.parse(localStorage.getItem(HIGH_SCORES)) || [];
+
+  const highScoreList = document.getElementById(HIGH_SCORES);
+
+  highScoreList.innerHTML = highScores
+    .map((score) => `<li>${score.score} - ${score.name}`)
+    .join('');
+}
