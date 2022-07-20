b34edf016a56e3c8e0394df58070a69f39fd93fd
Yasuhito Takamiya <yasuhito@gmail.com>
2022-07-20 13:34:08 +0900

    テストを追加

--- test/gate.spec.js
+++ test/gate.spec.js
@@ -10,13 +10,42 @@ describe('Gate', () => {
     })
 
     it('should have its own unique shape', function () {
-      const gate = new Gate('H');
+      const hGate = new Gate('H');
+      const xGate = new Gate('X');
+      const yGate = new Gate('Y');
+      const zGate = new Gate('Z');
+      const sGate = new Gate('S');
+      const tGate = new Gate('T');
 
-      expect(gate.shape).toEqual([
+      expect(hGate.shape).toEqual([
         ['I', 'I', 'I', 'I'],
         ['H', 'H', 'H', 'H'],
         ['I', 'I', 'I', 'I'],
         ['I', 'I', 'I', 'I']
+      ]);
+      expect(xGate.shape).toEqual([
+        ['X', 'I', 'I'],
+        ['X', 'X', 'X'],
+        ['I', 'I', 'I']
+      ]);
+      expect(yGate.shape).toEqual([
+        ['I', 'I', 'Y'],
+        ['Y', 'Y', 'Y'],
+        ['I', 'I', 'I']
+      ]);
+      expect(zGate.shape).toEqual([
+        ['Z', 'Z'],
+        ['Z', 'Z']
+      ]);
+      expect(sGate.shape).toEqual([
+        ['I', 'S', 'S'],
+        ['S', 'S', 'I'],
+        ['I', 'I', 'I']
+      ]);
+      expect(tGate.shape).toEqual([
+        ['I', 'T', 'I'],
+        ['T', 'T', 'T'],
+        ['I', 'I', 'I']
       ]);
     })
 
