30c42c0a129ecf5d4b1fad43cb3ab5b43b8f4d52
Yasuhito Takamiya <yasuhito@gmail.com>
2022-07-20 13:46:40 +0900

    pretty-quick を追加

--- test/gate.spec.js
+++ test/gate.spec.js
@@ -1,99 +1,99 @@
-import {Gate} from '../src/gate.js';
-import {getBoardCanvasContext} from '../src/constants.js';
+import { Gate } from "../src/gate.js";
+import { getBoardCanvasContext } from "../src/constants.js";
 
-describe('Gate', () => {
-  describe('new', () => {
-    it('should have name', function () {
-      const gate = new Gate('H');
+describe("Gate", () => {
+  describe("new", () => {
+    it("should have name", function () {
+      const gate = new Gate("H");
 
-      expect(gate.name).toEqual('H');
-    })
+      expect(gate.name).toEqual("H");
+    });
 
-    it('should throw an exception when an invalid name is passed', function () {
+    it("should throw an exception when an invalid name is passed", function () {
       expect(() => {
-        new Gate('?');
-      }).toThrow('Invalid gate name: ?');
-    })
+        new Gate("?");
+      }).toThrow("Invalid gate name: ?");
+    });
 
-    it('should have its own unique shape', function () {
-      const hGate = new Gate('H');
-      const xGate = new Gate('X');
-      const yGate = new Gate('Y');
-      const zGate = new Gate('Z');
-      const sGate = new Gate('S');
-      const tGate = new Gate('T');
+    it("should have its own unique shape", function () {
+      const hGate = new Gate("H");
+      const xGate = new Gate("X");
+      const yGate = new Gate("Y");
+      const zGate = new Gate("Z");
+      const sGate = new Gate("S");
+      const tGate = new Gate("T");
 
       expect(hGate.shape).toEqual([
-        ['I', 'I', 'I', 'I'],
-        ['H', 'H', 'H', 'H'],
-        ['I', 'I', 'I', 'I'],
-        ['I', 'I', 'I', 'I']
+        ["I", "I", "I", "I"],
+        ["H", "H", "H", "H"],
+        ["I", "I", "I", "I"],
+        ["I", "I", "I", "I"],
       ]);
       expect(xGate.shape).toEqual([
-        ['X', 'I', 'I'],
-        ['X', 'X', 'X'],
-        ['I', 'I', 'I']
+        ["X", "I", "I"],
+        ["X", "X", "X"],
+        ["I", "I", "I"],
       ]);
       expect(yGate.shape).toEqual([
-        ['I', 'I', 'Y'],
-        ['Y', 'Y', 'Y'],
-        ['I', 'I', 'I']
+        ["I", "I", "Y"],
+        ["Y", "Y", "Y"],
+        ["I", "I", "I"],
       ]);
       expect(zGate.shape).toEqual([
-        ['Z', 'Z'],
-        ['Z', 'Z']
+        ["Z", "Z"],
+        ["Z", "Z"],
       ]);
       expect(sGate.shape).toEqual([
-        ['I', 'S', 'S'],
-        ['S', 'S', 'I'],
-        ['I', 'I', 'I']
+        ["I", "S", "S"],
+        ["S", "S", "I"],
+        ["I", "I", "I"],
       ]);
       expect(tGate.shape).toEqual([
-        ['I', 'T', 'I'],
-        ['T', 'T', 'T'],
-        ['I', 'I', 'I']
+        ["I", "T", "I"],
+        ["T", "T", "T"],
+        ["I", "I", "I"],
       ]);
-    })
+    });
 
-    it('should have its own unique color', function () {
-      const hGate = new Gate('H');
-      const xGate = new Gate('X');
-      const yGate = new Gate('Y');
-      const zGate = new Gate('Z');
-      const sGate = new Gate('S');
-      const tGate = new Gate('T');
+    it("should have its own unique color", function () {
+      const hGate = new Gate("H");
+      const xGate = new Gate("X");
+      const yGate = new Gate("Y");
+      const zGate = new Gate("Z");
+      const sGate = new Gate("S");
+      const tGate = new Gate("T");
 
-      expect(hGate.color).toEqual('cyan');
-      expect(xGate.color).toEqual('blue');
-      expect(yGate.color).toEqual('orange');
-      expect(zGate.color).toEqual('green');
-      expect(sGate.color).toEqual('purple');
-      expect(tGate.color).toEqual('red');
-    })
+      expect(hGate.color).toEqual("cyan");
+      expect(xGate.color).toEqual("blue");
+      expect(yGate.color).toEqual("orange");
+      expect(zGate.color).toEqual("green");
+      expect(sGate.color).toEqual("purple");
+      expect(tGate.color).toEqual("red");
+    });
 
-    it('should initialize x and y with 0', function () {
-      const gate = new Gate('H');
+    it("should initialize x and y with 0", function () {
+      const gate = new Gate("H");
 
-      expect(gate.x).toBe(0)
-      expect(gate.y).toBe(0)
-    })
-  })
+      expect(gate.x).toBe(0);
+      expect(gate.y).toBe(0);
+    });
+  });
 
-  describe('draw', () => {
-    beforeEach(function() {
-      const board = document.createElement('canvas');
-      board.id = 'board'
+  describe("draw", () => {
+    beforeEach(function () {
+      const board = document.createElement("canvas");
+      board.id = "board";
       document.body.appendChild(board);
     });
 
-    it('should draw its own shape', function () {
-      const gate = new Gate('H');
+    it("should draw its own shape", function () {
+      const gate = new Gate("H");
       const ctx = getBoardCanvasContext();
 
-      gate.draw(ctx)
+      gate.draw(ctx);
       const events = ctx.__getEvents();
 
       expect(events).toMatchSnapshot();
-    })
-  })
-})
+    });
+  });
+});
