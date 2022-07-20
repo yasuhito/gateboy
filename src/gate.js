aad0d98f8ffe72f7ce3e4e94219221557d1a829c
Yasuhito Takamiya <yasuhito@gmail.com>
2022-07-21 05:24:52 +0900

    テストを追加

--- src/gate.js
+++ src/gate.js
@@ -1,21 +1,45 @@
-const NAMES = ['H', 'X', 'Y', 'Z', 'S', 'T']
+const NAMES = ["H", "X", "Y", "Z", "S", "T"];
 const SHAPES = [
-  [['I', 'I', 'I', 'I'], ['H', 'H', 'H', 'H'], ['I', 'I', 'I', 'I'], ['I', 'I', 'I', 'I']],
-  [['X', 'I', 'I'], ['X', 'X', 'X'], ['I', 'I', 'I']],
-  [['I', 'I', 'Y'], ['Y', 'Y', 'Y'], ['I', 'I', 'I']],
-  [['Z', 'Z'], ['Z', 'Z']],
-  [['I', 'S', 'S'], ['S', 'S', 'I'], ['I', 'I', 'I']],
-  [['I', 'T', 'I'], ['T', 'T', 'T'], ['I', 'I', 'I']],
+  [
+    ["I", "I", "I", "I"],
+    ["H", "H", "H", "H"],
+    ["I", "I", "I", "I"],
+    ["I", "I", "I", "I"],
+  ],
+  [
+    ["X", "I", "I"],
+    ["X", "X", "X"],
+    ["I", "I", "I"],
+  ],
+  [
+    ["I", "I", "Y"],
+    ["Y", "Y", "Y"],
+    ["I", "I", "I"],
+  ],
+  [
+    ["Z", "Z"],
+    ["Z", "Z"],
+  ],
+  [
+    ["I", "S", "S"],
+    ["S", "S", "I"],
+    ["I", "I", "I"],
+  ],
+  [
+    ["I", "T", "I"],
+    ["T", "T", "T"],
+    ["I", "I", "I"],
+  ],
   // [[7, 7, 'I'], ['I', 7, 7], ['I', 'I', 'I']]
 ];
-const COLORS = ['cyan', 'blue', 'orange', 'green', 'purple', 'red'];
+const COLORS = ["cyan", "blue", "orange", "green", "purple", "red"];
 
 export class Gate {
   static random() {
     const typeId = this.randomizeGateType();
-    const gate = new Gate(this.NAMES[typeId])
+    const gate = new Gate(this.NAMES[typeId]);
 
-    return gate
+    return gate;
   }
 
   // TODO: implement TGM (The Grand Master series) randomizer
@@ -37,10 +61,10 @@ export class Gate {
   }
 
   constructor(name) {
-    const typeId = Gate.NAMES.indexOf(name)
-    if (typeId === -1) throw new Error(`Invalid gate name: ${name}`)
+    const typeId = Gate.NAMES.indexOf(name);
+    if (typeId === -1) throw new Error(`Invalid gate name: ${name}`);
 
-    this.name = name
+    this.name = name;
     this.shape = Gate.SHAPES[typeId];
     this.color = Gate.COLORS[typeId];
     this.x = 0;
@@ -50,15 +74,15 @@ export class Gate {
   draw(ctx) {
     this.shape.forEach((row, y) => {
       row.forEach((value, x) => {
-        if (value === 'I') return;
+        if (value === "I") return;
 
         // draw gate rectangle
         ctx.fillStyle = this.color;
         ctx.fillRect(this.x + x, this.y + y, 1, 1);
 
         // draw gate name
-        ctx.fillStyle = '#000';
-        ctx.font = '1px sans-serif';
+        ctx.fillStyle = "#000";
+        ctx.font = "1px sans-serif";
         ctx.fillText(this.name, this.x + x, this.y + y + 1);
       });
     });
--- test/board.spec.js
+++ test/board.spec.js
@@ -12,9 +12,15 @@ describe("Gate", () => {
   });
 
   describe("new", function () {
+    let ctx = null;
+    let ctxNext = null;
+
+    beforeEach(function () {
+      ctx = Board.createBoardCanvasContext();
+      ctxNext = Board.createNextCanvasContext();
+    });
+
     it("should initialize grid with I", function () {
-      const ctx = Board.createBoardCanvasContext();
-      const ctxNext = Board.createNextCanvasContext();
       const board = new Board(ctx, ctxNext);
 
       expect(board.grid.length).toBe(Board.ROWS);
@@ -27,19 +33,91 @@ describe("Gate", () => {
     });
 
     it("should set the next gate", function () {
-      const ctx = Board.createBoardCanvasContext();
-      const ctxNext = Board.createNextCanvasContext();
       const board = new Board(ctx, ctxNext);
 
       expect(board.nextGate).toBeInstanceOf(Gate);
     });
 
     it("should set the current gate", function () {
-      const ctx = Board.createBoardCanvasContext();
-      const ctxNext = Board.createNextCanvasContext();
       const board = new Board(ctx, ctxNext);
 
       expect(board.gate).toBeInstanceOf(Gate);
+    });
+  });
+
+  describe("rotate", function () {
+    let board = null;
+
+    beforeEach(function () {
+      const ctx = Board.createBoardCanvasContext();
+      const ctxNext = Board.createNextCanvasContext();
+      board = new Board(ctx, ctxNext);
+    });
+
+    it("should rotate H gate block", function () {
+      const gate = new Gate("H");
+      const rotatedGate = board.rotate(gate);
+
+      expect(rotatedGate.shape).toEqual([
+        ["I", "I", "H", "I"],
+        ["I", "I", "H", "I"],
+        ["I", "I", "H", "I"],
+        ["I", "I", "H", "I"],
+      ]);
+    });
+
+    it("should rotate X gate block", function () {
+      const gate = new Gate("X");
+      const rotatedGate = board.rotate(gate);
+
+      expect(rotatedGate.shape).toEqual([
+        ["I", "X", "X"],
+        ["I", "X", "I"],
+        ["I", "X", "I"],
+      ]);
+    });
+
+    it("should rotate Y gate block", function () {
+      const gate = new Gate("Y");
+      const rotatedGate = board.rotate(gate);
+
+      expect(rotatedGate.shape).toEqual([
+        ["I", "Y", "I"],
+        ["I", "Y", "I"],
+        ["I", "Y", "Y"],
+      ]);
+    });
+
+    it("should rotate Z gate block", function () {
+      const gate = new Gate("Z");
+      const rotatedGate = board.rotate(gate);
+
+      expect(rotatedGate.shape).toEqual([
+        ["Z", "Z"],
+        ["Z", "Z"],
+      ]);
+    });
+
+    it("should rotate S gate block", function () {
+      const gate = new Gate("S");
+      const rotatedGate = board.rotate(gate);
+
+      expect(rotatedGate.shape).toEqual([
+        ["I", "S", "I"],
+        ["I", "S", "S"],
+        ["I", "I", "S"],
+      ]);
+    });
+
+    it("should rotate T gate block", function () {
+      const gate = new Gate("T");
+      const rotatedGate = board.rotate(gate);
+
+      expect(rotatedGate.shape).toEqual([
+        ["I", "T", "I"],
+        ["I", "T", "T"],
+        ["I", "T", "I"],
+      ]);
     });
   });
 });
